042d24e7121611b8e7021d7f8de461cedf64204c
Bhim84 <37284961+Bhim84@users.noreply.github.com>
2018-06-14 10:38:14 +0530

    testing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ReactDom from "react-dom";
-import Item from "./item"
+import Item from "./item";
+import "./index.css";
 
 class Board extends Component {
   constructor(props) {
@@ -9,22 +10,42 @@ class Board extends Component {
       boardMap: this.getBoardMap()
     };
     this.getBoardMap = this.getBoardMap.bind(this);
+    this.catch = this.catch.bind(this);
+  }
+
+  componentWillMount() {
+    this.style = {
+      position: "relative",
+      left: window.innerWidth / 2 - this.state.boxSize * 3 / 2 + "px",
+      top: window.innerHeight / 2 - this.state.boxSize * 3 / 2 + "px",
+      width: "310px",
+      margin: "1px 1px 0px 0px"
+    };
   }
 
   getBoardMap() {
     const tempMap = [];
-    for (var i = 1; i < 10; i++) {
-      tempMap.push({ id: i, value: Math.floor(Math.random() * 100 + 1) });
+    for (var i = 1; i <= 25; i++) {
+      tempMap.push({ id: i, value: Math.floor(Math.random() * 1000 + 1) });
     }
     return tempMap;
   }
+
+  catch(max) {
+    alert(max);
+  }
+
   render() {
     return (
-      <div>
-        <ul class="flex-container wrap">
-          {this.state.map((key,value)=> ())}
+      <div className="board">
+        <ul className="flex-container wrap fix">
+          {this.state.boardMap.map(key => (
+            <Item value={key.value} onClick={this.catch} />
+          ))}
         </ul>
       </div>
     );
   }
 }
+
+ReactDom.render(<Board />, document.getElementById("root"));
